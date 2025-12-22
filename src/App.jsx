b51d141e70a02eaf1b7d28d91467a98a4b2f5c3d
Pritam Halder <halderpritamhalder1@gmail.com>
2025-12-22 18:01:21 +0530

    implement share resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
+import { counterStore } from "remoteButton/counterStore";
 
 const RemoteButton = React.lazy(() => import("remoteButton/Button"));
 const RemoteCard = React.lazy(() => import("remoteCard/Card"));
 
 const App = () => {
+  const [count, setCount] = useState(counterStore.getCount());
+
+  useEffect(() => {
+    return counterStore.subscribe(setCount);
+  }, []);
+
   return (
     <div>
       <h1>Host Application</h1>
+      <h2>Shared Count in Host: {count}</h2>
 
       <Suspense fallback={<div>Loading Button...</div>}>
         <RemoteButton />
